a9426776e12b4e47d4e0d6769e902d7cf5106b0e
Tobias Ganzhorn <tobias.ganzhorn@csi-online.de>
2025-06-23 09:27:03 +0200

    fix: register each controller as plugin

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifySchema } from "fastify";
 import { ControllerCtx } from "./controller/controller.js";
 import { createCtx } from "./controller/ctx.js";
 import { createInjectorFn } from "./controller/injector.js";
@@ -52,54 +52,62 @@ export function registerControllers<
   const builtControllers = buildControllers(controllers);
 
   for (const controller of builtControllers) {
-    const { routerCtxs, rootPath } = Reflect.getMetadata(
-      "controller:config",
-      controller
-    ) as ControllerCtx;
+    fastify.register((fastify, {}) => {
+      const { routerCtxs, rootPath } = Reflect.getMetadata(
+        "controller:config",
+        controller
+      ) as ControllerCtx;
 
-    for (const [, routerCtx] of routerCtxs.entries()) {
-      const injectorFn = createInjectorFn(routerCtx);
+      for (const [, routerCtx] of routerCtxs.entries()) {
+        const injectorFn = createInjectorFn(routerCtx);
 
-      const payload = [
-        rootPath + routerCtx.path,
-        routerCtx.opts
-          ? {
-              ...routerCtx.opts,
-              schema: routerCtx.schema,
-            }
-          : {
-              schema: routerCtx.schema,
-            },
-        async (request: any, reply: any) => {
-          const ctx = createCtx(request, reply, routerCtx);
+        const payload = [
+          rootPath + routerCtx.path,
+          routerCtx.opts
+            ? {
+                ...routerCtx.opts,
+                schema: {
+                  tags: [rootPath],
+                  ...routerCtx.schema,
+                } as FastifySchema,
+              }
+            : {
+                schema: {
+                  tags: [rootPath],
+                  ...routerCtx.schema,
+                } as FastifySchema,
+              },
+          async (request: any, reply: any) => {
+            const ctx = createCtx(request, reply, routerCtx);
 
-          return injectorFn(
-            controller[routerCtx.propertyKey].bind(controller),
-            ctx
-          );
-        },
-      ] as const;
+            return injectorFn(
+              controller[routerCtx.propertyKey].bind(controller),
+              ctx
+            );
+          },
+        ] as const;
 
-      switch (routerCtx.method) {
-        case "GET":
-          fastify.get(...payload);
-          break;
-        case "DELETE":
-          fastify.delete(...payload);
-          break;
-        case "PATCH":
-          fastify.patch(...payload);
-          break;
-        case "POST":
-          fastify.post(...payload);
-          break;
-        case "PUT":
-          fastify.put(...payload);
-          break;
-        case "ALL":
-          fastify.all(...payload);
-          break;
+        switch (routerCtx.method) {
+          case "GET":
+            fastify.get(...payload);
+            break;
+          case "DELETE":
+            fastify.delete(...payload);
+            break;
+          case "PATCH":
+            fastify.patch(...payload);
+            break;
+          case "POST":
+            fastify.post(...payload);
+            break;
+          case "PUT":
+            fastify.put(...payload);
+            break;
+          case "ALL":
+            fastify.all(...payload);
+            break;
+        }
       }
-    }
+    });
   }
 }
